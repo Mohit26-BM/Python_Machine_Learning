435c0d55bbea45d77bce1ab8c91456f35fb45b75
Mohit Bali <m26ohitbali@gmail.com>
2026-02-15 16:42:19 +0530

    Refactor model loading and feature extraction

--- Customer_Churn/app.py
+++ Customer_Churn/app.py
@@ -12,32 +12,11 @@ app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-model = pickle.load(open(os.path.join(BASE_DIR, "customer_churn_model.pkl"), "rb"))
-#model_data = pickle.load(open(os.path.join(BASE_DIR, "customer_churn_model.pkl"), "rb"))
-#feature_names = model_data["feature_names"]
-feature_names = [
-    "gender",
-    "SeniorCitizen",
-    "Partner",
-    "Dependents",
-    "tenure",
-    "PhoneService",
-    "MultipleLines",
-    "InternetService",
-    "OnlineSecurity",
-    "OnlineBackup",
-    "DeviceProtection",
-    "TechSupport",
-    "StreamingTV",
-    "StreamingMovies",
-    "Contract",
-    "PaperlessBilling",
-    "PaymentMethod",
-    "MonthlyCharges",
-    "TotalCharges"
-]
-encoders = pickle.load(open(os.path.join(BASE_DIR, "encoders.pkl"), "rb"))
+model_data = pickle.load(open(os.path.join(BASE_DIR, "customer_churn_model.pkl"), "rb"))
 
+model = model_data["model"]
+feature_names = model_data["feature_names"]
+encoders = pickle.load(open(os.path.join(BASE_DIR, "encoders.pkl"), "rb"))
 
 # -------------------------------
 # Database Functions
@@ -178,4 +157,5 @@ def dashboard():
 init_db()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 

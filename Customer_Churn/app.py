fa03657b542377e7a5a77d4f71929db040cfc7bb
Mohit Bali <m26ohitbali@gmail.com>
2026-02-15 15:59:01 +0530

    Refactor feature names and model data loading
    Updated feature names directly in the code and commented out model data loading.

--- Customer_Churn/app.py
+++ Customer_Churn/app.py
@@ -13,8 +13,29 @@ app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 model = pickle.load(open(os.path.join(BASE_DIR, "xgb_churn.pkl"), "rb"))
-model_data = pickle.load(open(os.path.join(BASE_DIR, "customer_churn_model.pkl"), "rb"))
-feature_names = model_data["feature_names"]
+#model_data = pickle.load(open(os.path.join(BASE_DIR, "customer_churn_model.pkl"), "rb"))
+#feature_names = model_data["feature_names"]
+feature_names = [
+    "gender",
+    "SeniorCitizen",
+    "Partner",
+    "Dependents",
+    "tenure",
+    "PhoneService",
+    "MultipleLines",
+    "InternetService",
+    "OnlineSecurity",
+    "OnlineBackup",
+    "DeviceProtection",
+    "TechSupport",
+    "StreamingTV",
+    "StreamingMovies",
+    "Contract",
+    "PaperlessBilling",
+    "PaymentMethod",
+    "MonthlyCharges",
+    "TotalCharges"
+]
 encoders = pickle.load(open(os.path.join(BASE_DIR, "encoders.pkl"), "rb"))
 
 
@@ -158,3 +179,4 @@ def dashboard():
 if __name__ == "__main__":
     init_db()
     app.run(host="0.0.0.0", port=5000)
+
